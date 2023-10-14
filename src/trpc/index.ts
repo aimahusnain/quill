d405385bb817d1fa32213e90bb4c0ea35fae2137
Muhammad Husnain <aimahusnain@gmail.com>
2023-10-14 06:14:11 -0700

    Upgrade

---
+++ src/trpc/index.ts
@@ -0,0 +1,52 @@
+'use client'
+
+import { db } from '@/db'
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import { TRPCError } from '@trpc/server'
+import {
+  privateProcedure,
+  publicProcedure,
+  router,
+} from './trpc'
+
+export const appRouter = router({
+  authCallback: publicProcedure.query(async () => {
+    const { getUser } = getKindeServerSession()
+    const user = getUser()
+
+    if (!user.id || !user.email)
+      throw new TRPCError({ code: 'UNAUTHORIZED' })
+
+    // check if the user is in the database
+    const dbUser = await db.user.findFirst({
+      where: {
+        id: user.id,
+      },
+    })
+
+    if (!dbUser) {
+      // create user in db
+      await db.user.create({
+        data: {
+          id: user.id,
+          email: user.email,
+        },
+      })
+    }
+
+    return { success: true }
+  }),
+    
+    
+getUserFiles: privateProcedure.query(async ({ ctx }) => {
+    const { userId } = ctx
+
+    return await db.file.findMany({
+      where: {
+        userId,
+      },
+    })
+}),
+})
+
+export type AppRouter = typeof appRouter;
